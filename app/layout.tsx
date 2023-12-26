105fb379cc434205568a5f1f6220b976b0694d63
Wuilmer Carbajal <wcarbajal@outlook.com>
2023-12-26 13:27:45 -0500

    mañana2 del 26.12.2023

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import NavBar from '../components/navbar/NavBar';
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 
-import NavBar from '../components/navbar/NavBar'
 import Image from 'next/image'
+import Link from 'next/link'
+
 
 export default function HomePage() {
   return (
-    <>
-    <NavBar />
+    
+    
     <main className="flex flex-col items-center p-24">
       <span className='text-5xl'>Hola Mundo</span>
+      <Link href='/about'>About page</Link>
     </main>
-    </>
+    
   )
 }
